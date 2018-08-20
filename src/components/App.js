f8094623383b9b8876cf82222cbc62df9aa24554
Unknown <fivehoho75@gmail.com>
2018-08-20 23:36:18 +0900

    redux init

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+
+const App = () => (
+  <React.Fragment>
+    Hello!!
+  </React.Fragment>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
